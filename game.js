5096600aada0800c46a03bd82c3de456aa011163
Batuhan Ayırtman <batuayirtman@gmail.com>
2021-01-29 15:15:04 +0300

    levels added

--- game.js
+++ game.js
@@ -1,13 +1,39 @@
 var gamePattern = [];
-
+var userClickedPattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
+var level = 0;
+
+$(document).one("keypress",nextSequence);
+
 
 function nextSequence(){
-  var randomNumber = Math.round(Math.random() * 3);
+
+  var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonColors[randomNumber];
 
-  $("#"+ randomChosenColor).fadeToggle();
-  
+  $("."+ randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
   gamePattern.push(randomChosenColor);
 
+  playSound(randomChosenColor);
+
+  level++;
+
+  $("#level-title").html("Level " + level);
 };
+
+$(".btn").click(function(event){
+  var userChosenColour = event.target.id;
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+});
+
+function playSound(name){
+  var audio = new Audio("./sounds/" + name + ".mp3");
+  audio.play();
+};
+
+function animatePress(currentColor){
+  $("." + currentColor.id).addClass("pressed");
+  setTimeout(function(){$("." + currentColor.id).removeClass("pressed")}, 100);
+}
